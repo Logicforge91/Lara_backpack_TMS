01fa6a42f030d2c904aed36be069356c3901558e
sumanks1307 <12345678+Logicforge91@users.noreply.github.com>
2025-11-16 16:09:26 +0530

    dd

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,10 +1,27 @@
-{{-- This file is used for menu items by any Backpack v6 theme --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+{{-- Dashboard --}}
+<li class="nav-item">
+    <a class="nav-link" href="{{ backpack_url('dashboard') }}">
+        <i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}
+    </a>
+</li>
 
-<x-backpack::menu-item title="Users" icon="la la-question" :link="backpack_url('user')" />
-<x-backpack::menu-item title="Teams" icon="la la-question" :link="backpack_url('team')" />
-<x-backpack::menu-item title="Employees" icon="la la-question" :link="backpack_url('employee')" />
-<x-backpack::menu-item title="Current releases" icon="la la-question" :link="backpack_url('current-release')" />
-<x-backpack::menu-item title="Completed releases" icon="la la-question" :link="backpack_url('completed-release')" />
-<x-backpack::menu-item title="Tasks" icon="la la-question" :link="backpack_url('task')" />
-<x-backpack::menu-item title="Monthly reports" icon="la la-question" :link="backpack_url('monthly-report')" />
+{{-- Users --}}
+<x-backpack::menu-item title="Users" icon="la la-users" :link="backpack_url('user')" />
+
+{{-- Teams --}}
+<x-backpack::menu-item title="Teams" icon="la la-sitemap" :link="backpack_url('team')" />
+
+{{-- Employees --}}
+<x-backpack::menu-item title="Employees" icon="la la-user-tie" :link="backpack_url('employee')" />
+
+{{-- Current Releases --}}
+<x-backpack::menu-item title="Current releases" icon="la la-rocket" :link="backpack_url('current-release')" />
+
+{{-- Completed Releases --}}
+<x-backpack::menu-item title="Completed releases" icon="la la-check-circle" :link="backpack_url('completed-release')" />
+
+{{-- Tasks --}}
+<x-backpack::menu-item title="Tasks" icon="la la-tasks" :link="backpack_url('task')" />
+
+{{-- Monthly Reports --}}
+<x-backpack::menu-item title="Monthly reports" icon="la la-file-alt" :link="backpack_url('monthly-report')" />
